d1a62665e345f9970ed63a96ccdcd974d06138bd
caickdias <caickandrade0@gmail.com>
2022-11-29 04:05:48 -0300

    added login, logout, user info

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import './App.css';
+import { useAuth0 } from '@auth0/auth0-react';
 
 function App() {
+
+  const { loginWithRedirect, user, isAuthenticated, isLoading, logout } = useAuth0();
+
+  if(isLoading){
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="App">
-      opa
+      <button
+        onClick={() => loginWithRedirect()}
+      >
+        Log In
+      </button>
+      { isAuthenticated && (
+        <div>
+          <img src={user.picture} alt={user.name} />
+          <h2>{user.name}</h2>
+          <p>{user.email}</p>
+        </div>)
+      }
+
+      <button onClick={() => logout({ returnTo: window.location.origin })}>
+        Log Out
+      </button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,9 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Auth0Provider } from '@auth0/auth0-react';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Auth0Provider
+    domain="dev-ybjw0sakngae46jb.us.auth0.com"
+    clientId="byHmG5apucZK5PCr1aMF7S1rhfSmDYh2"
+    redirectUri={window.location.origin}
+  >
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Auth0Provider>
 );
